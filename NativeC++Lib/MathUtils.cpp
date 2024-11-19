ea0fbee2e355d2f505917918e5deae184810fff4
praveens3 <praveen.s3@outlook.in>
2024-11-19 22:45:37 +0530

    Initial commit

---
+++ NativeC++Lib/MathUtils.cpp
@@ -0,0 +1,12 @@
+// MathUtils.cpp
+
+#include "MathUtils.h"
+
+int MathUtils::Add(int a, int b) {
+    return a + b;
+}
+
+double MathUtils::Add(double a, double b) {
+    return a + b;
+}
+
---
+++ NativeC++Lib/MathUtils.h
@@ -0,0 +1,14 @@
+#ifndef MATHUTILS_H
+#define MATHUTILS_H
+
+class MathUtils
+{
+public:
+    MathUtils() = default;
+    ~MathUtils() = default;
+
+    int Add(int a, int b);
+    double Add(double a, double b);
+};
+
+#endif // MATHUTILS_H
